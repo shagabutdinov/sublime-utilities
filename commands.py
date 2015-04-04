a07a7ad56db82c1b38254a69e8e31fd51e3a0db7
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2015-04-04 17:28:35 +0600

    minor improvments

--- commands.py
+++ commands.py
@@ -9,12 +9,14 @@ class ReplaceText(sublime_plugin.TextCommand):
 class ReplaceTextByRegexp(sublime_plugin.TextCommand):
   def run(self, edit, regexp, replacement, text_type = 'preceding_begin'):
     for sel in reversed(self.view.sel()):
-      if text_type != 'preceding_begin':
-        raise Exception('Text type should be "preceding_begin"; other types ' +
-          'not supported yet; you can add support for other types at ' +
-          'Utilities/commands.py')
 
-      region = sublime.Region(self.view.line(sel.begin()).a, sel.begin())
+      if text_type == 'preceding_begin':
+        region = sublime.Region(self.view.line(sel.begin()).a, sel.begin())
+      elif text_type == 'following_end':
+        region = sublime.Region(sel.end(), self.view.line(sel.end()).b)
+      else:
+        raise Exception('Unkonwn text type should be "' + text_type + '"')
+
       initial = self.view.substr(region)
       text = re.sub(regexp, replacement, initial)
       if text != initial:
@@ -28,3 +30,16 @@ class DeleteSelection(sublime_plugin.TextCommand):
   def run(self, edit):
     for sel in reversed(self.view.sel()):
       self.view.erase(edit, sel)
+
+class DeleteNextLine(sublime_plugin.TextCommand):
+  def run(self, edit):
+    for sel in reversed(self.view.sel()):
+      line = self.view.line(sel)
+      next_line = self.view.line(line.b + 1)
+      self.view.erase(edit, sublime.Region(next_line.a - 1, next_line.b))
+
+class PrintCurrentLine(sublime_plugin.TextCommand):
+  def run(self, edit):
+    for sel in reversed(self.view.sel()):
+      line = self.view.line(sel)
+      print(self.view.substr(line))
